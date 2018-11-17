a8b83a7ee7bc06798e7c0b452d9ef2cfc78b483d
Ross <rosstitute@gmail.com>
2018-11-17 20:08:12 +0000

    Updated whitespacing

--- example.listChannels.php
+++ example.listChannels.php
@@ -14,18 +14,18 @@ try {
 	$channels = $lnd->request('channels')->channels;
 
 	foreach($channels as $channel){
-		echo $channel->active 			. "\n";
+		echo $channel->active 		. "\n";
 		echo $channel->remote_pubkey 	. "\n";
 		echo $channel->channel_point 	. "\n";
-		echo $channel->chan_id 			. "\n";
-		echo $channel->capacity 		. "\n";
+		echo $channel->chan_id 		. "\n";
+		echo $channel->capacity 	. "\n";
 		echo $channel->local_balance 	. "\n";
 		echo $channel->remote_balance 	. "\n";
-		echo $channel->commit_fee		. "\n";
+		echo $channel->commit_fee	. "\n";
 		echo $channel->commit_weight 	. "\n";
-		echo $channel->fee_per_kw 		. "\n";
-		echo $channel->num_updates 		. "\n";
-		echo $channel->csv_delay	 	. "\n";
+		echo $channel->fee_per_kw 	. "\n";
+		echo $channel->num_updates 	. "\n";
+		echo $channel->csv_delay	. "\n";
 	}
 
 }catch (Exception $e){
