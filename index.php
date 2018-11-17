4d7147e29c792dfbd517dce824bdbd1415b2ce70
RSTMSN <rosstitute@gmail.com>
2018-11-17 20:03:05 +0000

    Updated formatting

--- index.php
+++ index.php
@@ -13,17 +13,17 @@ $lnd->loadMacaroon('../../admin.macaroon');
 try {
 	$info = $lnd->request('getinfo');
 
-	echo $info->identity_pubkey			. "\n";
-	echo $info->alias					. "\n";
-	echo $info->num_active_channels		. "\n";
-	echo $info->num_peers				. "\n";
-	echo $info->block_height			. "\n";
-	echo $info->block_hash				. "\n";
-	echo $info->synced_to_chain			. "\n";
-	echo $info->testnet					. "\n";
-	echo $info->synced_to_chain			. "\n";
-	echo $info->best_header_timestamp 	. "\n";
-	echo $info->version					. "\n";
+	echo $info->identity_pubkey	      . "\n";
+	echo $info->alias                 . "\n";
+	echo $info->num_active_channels	  . "\n";
+	echo $info->num_peers             . "\n";
+	echo $info->block_height          . "\n";
+	echo $info->block_hash            . "\n";
+	echo $info->synced_to_chain	      . "\n";
+	echo $info->testnet	              . "\n";
+	echo $info->synced_to_chain       . "\n";
+	echo $info->best_header_timestamp . "\n";
+	echo $info->version	              . "\n";
 
 	foreach($info->chains as $chain){
 		echo $chain . "\n";
