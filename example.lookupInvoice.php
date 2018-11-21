2023ddf4f7ba6ee303acfee3e1be99a35d6b78e5
Ross <rosstitute@gmail.com>
2018-11-21 18:12:18 +0000

    Update example.lookupInvoice.php

--- example.lookupInvoice.php
+++ example.lookupInvoice.php
@@ -15,15 +15,15 @@ $rHash = bin2hex(base64_decode("gC2FK31DaPYBRmIZMVvywvoqcLAiUsqVjkc7i7t7mhU="));
 try {
 	$invoice = $lnd->request('invoice/' . $rHash);
 
-	echo $invoice->memo 			. "\n";
-	echo $invoice->r_preimage 		. "\n";
-	echo $invoice->r_hash 			. "\n";
-	echo $invoice->value 			. "\n";
+	echo $invoice->memo 		. "\n";
+	echo $invoice->r_preimage 	. "\n";
+	echo $invoice->r_hash 		. "\n";
+	echo $invoice->value 		. "\n";
 	echo $invoice->creation_date 	. "\n";
 	echo $invoice->payment_request 	. "\n";
-	echo $invoice->expiry 			. "\n";
-	echo $invoice->cltv_expiry 		. "\n";
-	echo $invoice->add_index 		. "\n";
+	echo $invoice->expiry 		. "\n";
+	echo $invoice->cltv_expiry 	. "\n";
+	echo $invoice->add_index 	. "\n";
 
 	# if the invoice has not yet been settled, the lnd request response will not
 	# contain the 'settled' property. For consistency here, we create it and set
