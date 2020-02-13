f035cf12c32f2be9050d817053f57618b93e6cc2
Ross <rosstitute@gmail.com>
2020-02-13 01:23:10 +0000

    Update example.payInvoice.php

--- example.payInvoice.php
+++ example.payInvoice.php
@@ -8,7 +8,7 @@ $lnd->loadMacaroon('../../admin.macaroon');
 
 /*
  * Pay a Lightning Invoice
-  */
+ */
 
 $paymentOptions = array("payment_request" => "lntb15u1pd7mvympp5k4e3egmfrrszrgytlfdrn2anwwwrc97z89hsvsrx22ytsm59258sdq4xysyymr0vd4kzcmrd9hx7cqp2zlsxce8lqq34y9ldqfsp4atyzl4q9j6xv5g5qj733ul8k8mu3dt46y53y7c6kwe8spux4yr42wdk0g2rjlkzh2nm6zvcyd8klxadzpcq83nwhz");
 
